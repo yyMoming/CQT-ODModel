0883d6d9bfc26a79b337fc3bffc8a81589403020
ywm <980027517@qq.com>
2021-07-19 11:55:26 +0800

    修改了STFT，加了汉明窗

--- SpecTransform/include/_m_stft.h
+++ SpecTransform/include/_m_stft.h
@@ -8,73 +8,9 @@
 #include <complex>
 #include <vector>
 #include <fstream>
+#include <functional>
+#include <algorithm>
 namespace CQT{
-    template<class dtype>
-    void _m_stft(const dtype* y, const int& y_len, const int & n_fft, const int & hop_length, std::vector<std::complex<dtype> *> &stft_ret){
-        dtype *y_padded = new dtype[y_len + n_fft];
-        copy(y, y + y_len, y_padded + n_fft / 2);
-        int n_frames = floor(double(y_len + n_fft) / hop_length);
-//        vector<std::complex<dtype> *> stft_res;
-        fftw_complex *in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft), *out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft);
-        fftw_plan p;
-
-        for(int i = 0; i < n_frames; i++){
-            std::complex<dtype> *res = new std::complex<dtype>[n_fft / 2 + 1];
-            for(int k = 0; k < n_fft; k++){
-                in[k][0] = y_padded[i * hop_length + k];
-                in[k][1] = 0;
-            }
-            p = fftw_plan_dft_1d(n_fft, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftw_execute(p);
-//            res = reinterpret_cast<std::complex<double> *>(out);
-            memcpy(res, out, (n_fft / 2 + 1) * sizeof(std::complex<dtype>));
-            stft_ret.push_back(res);
-        }
-        fftw_destroy_plan(p);
-        fftw_free(in);
-        fftw_free(out);
-        delete[] y_padded;
-    }
-    /*
-     * 计算并输出STFT结果
-     * */
-    template<class dtype>
-    void _m_stft(const dtype* y, const int& y_len, const int & n_fft, const int & hop_length){
-        std::fstream fimag("_stft_imag.txt", std::ios::out);
-        std::fstream freal("_stft_real.txt", std::ios::out);
-        dtype *y_padded = new dtype[y_len + n_fft];
-        copy(y, y + y_len, y_padded + n_fft / 2);
-        int n_frames = floor(double(y_len + n_fft) / hop_length);
-        //        vector<std::complex<dtype> *> stft_res;
-        fftw_complex *in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft), *out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft);
-        fftw_plan p;
-        for(int i = 0; i < n_frames; i++){
-        //        copy(y_padded + i * n_fft, y_padded + (i + 1) * n_fft, in);
-        //            memcpy(in, y_padded + i * hop_length, n_fft * sizeof(fftw_complex));
-            for(int k = 0; k < n_fft; k++){
-                in[k][0] = y_padded[i * hop_length + k];
-                in[k][1] = 0;
-            }
-            p = fftw_plan_dft_1d(n_fft, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftw_execute(p);
-            for(int k = 0; k < n_fft; k++){
-                auto t = *(out + k);
-                if(k == n_fft - 1){
-                    fimag << t[1] << std::endl;
-                    freal << t[0] << std::endl;
-                    break;
-                }
-                fimag << t[1] << ",";
-                freal << t[0] << ",";
-
-            }
-        }
-        fftw_destroy_plan(p);
-        fftw_free(in);
-        fftw_free(out);
-        delete[] y_padded;
-    }
-
 
     template<class dtype>
     inline void hanningWindow(std::vector<std::complex<dtype>> &win, int win_size){
@@ -102,6 +38,86 @@ namespace CQT{
 //            cout << M_PI;
 //            win[i].imag = 0;
         }
+    }
+
+    template<class dtype>
+    void _m_stft(const dtype* y, const int& y_len, const int & n_fft, const int & hop_length, std::vector<std::complex<dtype> *> &stft_ret){
+        dtype *y_padded = new dtype[y_len + n_fft];
+        copy(y, y + y_len, y_padded + n_fft / 2);
+        int n_frames = floor(double(y_len) / hop_length);
+    //        vector<std::complex<dtype> *> stft_res;
+        fftw_complex *in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft), *out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft);
+        fftw_plan p;
+
+        for(int i = 0; i < n_frames; i++){
+            std::complex<dtype> *res = new std::complex<dtype>[n_fft / 2 + 1];
+            memset(res, 0, sizeof(std::complex<dtype>) * (n_fft / 2 + 1));
+            for(int k = 0; k < n_fft; k++){
+                in[k][0] = y_padded[i * hop_length + k];
+                in[k][1] = 0;
+            }
+            p = fftw_plan_dft_1d(n_fft, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+            fftw_execute(p);
+            //            res = reinterpret_cast<std::complex<double> *>(out);
+            memcpy(res, out, (n_fft / 2 + 1) * sizeof(std::complex<dtype>));
+            stft_ret.push_back(res);
+        }
+        fftw_destroy_plan(p);
+        fftw_free(in);
+        fftw_free(out);
+        delete[] y_padded;
+    }
+
+    /*
+     * 计算并输出STFT结果
+     * */
+    template<class dtype>
+    void _m_stft(const dtype* y, const int& y_len, const int & n_fft, const int & hop_length){
+        std::fstream fimag("_stft_imag.txt", std::ios::out);
+        std::fstream freal("_stft_real.txt", std::ios::out);
+        dtype *y_padded = new dtype[y_len + n_fft];
+        std::complex<dtype> *window = new std::complex<dtype>[n_fft];
+        hanningWindow(window, n_fft);
+        copy(y, y + y_len, y_padded + n_fft / 2);
+        int n_frames = floor(double(y_len) / hop_length);
+        //        vector<std::complex<dtype> *> stft_res;
+        fftw_complex *in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft), *out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * n_fft);
+        fftw_plan p;
+        std::complex<dtype> *ptr = nullptr;
+        for(int i = 0; i < n_frames; i++){
+            //        copy(y_padded + i * n_fft, y_padded + (i + 1) * n_fft, in);
+            //            memcpy(in, y_padded + i * hop_length, n_fft * sizeof(fftw_complex));
+            for(int k = 0; k < n_fft; k++){
+                in[k][0] = y_padded[i * hop_length + k];
+                in[k][1] = 0;
+            }
+            ptr = reinterpret_cast<std::complex<dtype> *>(in);
+            std::transform(ptr, ptr + n_fft, window, ptr, std::multiplies<std::complex<dtype>>());
+            p = fftw_plan_dft_1d(n_fft, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+            fftw_execute(p);
+            for(int k = 0; k < n_fft; k++){
+                auto t = *(out + k);
+                if(k == n_fft - 1){
+                    fimag << t[1] << std::endl;
+                    freal << t[0] << std::endl;
+                    break;
+                }else{
+                    fimag << t[1] << ",";
+                    freal << t[0] << ",";
+                }
+
+
+            }
+//            fimag << std::endl;
+//            freal << std::endl;
+        }
+        fimag.close();
+        freal.close();
+        fftw_destroy_plan(p);
+        fftw_free(in);
+        fftw_free(out);
+        delete[] y_padded;
+        delete[] window;
     }
 }
 #endif //SINGTALENT__M_STFT_H
